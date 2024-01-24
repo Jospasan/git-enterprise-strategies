22266624f179b78d4fb0b5439b15a79c5ab2926f
Joseph Schley <bbmaster08@gmail.com>
2024-01-23 21:52:33 -0500

    Fixed the gaterlogo

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
 <<<<<<< HEAD
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Joseph Schley and this is my project </p>
+
 =======
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is our team project.
-        </p>	
+        </p>
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Joseph Schley and this is my project </p>
 >>>>>>> 242861e (added new project text)
       </header>
     </div>
