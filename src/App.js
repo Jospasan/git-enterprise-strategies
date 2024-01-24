210fa5758d4075d702257886433bc2b34284ff61
Joseph Schley <bbmaster08@gmail.com>
2024-01-23 22:29:14 -0500

    Made Changes to have teammtes code

--- src/App.js
+++ src/App.js
@@ -5,22 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-
-=======
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is our team project.
-<<<<<<< HEAD
-        </p>
+        </p>	
 
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={gatorLogo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
         <p> My name is Joseph Schley and this is my project </p>
->>>>>>> 242861e (added new project text)
-=======
-        </p>	
->>>>>>> 242861e (added new project text)
       </header>
     </div>
   );
